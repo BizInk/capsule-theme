97115b90b1de0fb7a86330627481cb5dc604f2b0
Jayden Major <jaydenmajor@gmail.com>
2025-01-28 11:03:01 +1000

    Real Blog Update

--- home.php
+++ home.php
@@ -28,7 +28,7 @@ if (have_posts()) {
 				the_post();
 				$post_image = has_post_thumbnail() ? get_the_post_thumbnail_url() : get_stylesheet_directory_uri() . '/images/default.jpg';
 				?>
-				<div class="col-4 col-md-6 col-xl-4 team-member weekly-digest">
+				<div class="col-12 col-md-6 col-xl-4 team-member weekly-digest">
 					<div class="team-member-wrap">
 						<a href="<?php the_permalink(); ?>" class="member-img">
 						<img src="<?= $post_image; ?>" alt="<?php the_title(); ?>">
